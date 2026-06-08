87fa796d501961b7a52cf1f39723bf351ada4b7e
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-06-08 11:25:18 +0200

    Cons Enemy class, tower upgrades

--- Tower_Defence/Enemy.cs
+++ Tower_Defence/Enemy.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.MediaFoundation;
+using Spline;
 
 namespace Tower_Defence
 {
-    internal class Enemy
+    public class Enemy
     {
+        private Texture2D enemyTexture;
+        private Vector2 enemyPos;
+        private Rectangle enemyTexRec;
+        private int enemyHealth;
+        private int enemyDamage;
+        private int enemySpeed;
+        private int enemyLevel;
+        public Enemy(Texture2D tex, Vector2 pos, Rectangle texRec) // Constructor for the Enemy class
+        {
+            this.enemyTexture = tex;
+            this.enemyPos = pos;
+            this.enemyTexRec = texRec;
+            enemyLevel = 1;
+        }
     }
 }
--- Tower_Defence/Tower.cs
+++ Tower_Defence/Tower.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SharpDX.MediaFoundation;
 using Spline;
+using System;
 
 namespace Tower_Defence
 {
@@ -15,35 +16,59 @@ namespace Tower_Defence
         private int towerDamage;
         private int towerRange;
         private int towerLevel;
+        private string towerType;
 
-        public Tower(Texture2D tex, Vector2 pos, int range, Rectangle texRec) // Constructor for the Tower class
+        public Tower(Texture2D tex, Vector2 pos, Rectangle texRec, string type) // Constructor for the Tower class
         {
             this.towerTexture = tex;
             this.towerPos = pos;
-            this.towerRange = range;
+            this.towerRange = 0;
             this.towerTexRec = texRec;
+            this.towerType = type;
             this.towerLevel = 1;
-            towerHitbox = new Rectangle((int)range, (int)range, (int)range, (int)range); // Initialize the hitbox with the tower's range
+            towerHitbox = new Rectangle((int)towerRange, (int)towerRange, (int)towerRange, (int)towerRange); // Initialize the hitbox with the tower's range
+            towerDamage = 0;
         }
 
         public void DamageBasedOnLevel()
         {
             if (towerLevel == 1)
             {
-                towerDamage = 10;
+                towerDamage += 10;
+                towerRange += 10;
             }
             else if (towerLevel == 2)
             {
-                towerDamage = 20;
+                towerDamage += 20;
+                towerRange += 20;
             }
             else if (towerLevel == 3)
             {
-                towerDamage = 30;
+                towerDamage += 30;
+                towerRange += 30;
+            }
+        }
+
+        public void TowerType()
+        {
+            if (towerType == "Wooden") 
+            {
+                towerDamage = 10;
+                towerRange = 40;
+                towerHitbox = new Rectangle((int)towerPos.X - towerRange, (int)towerPos.Y - towerRange, (int)towerRange * 2, (int)towerRange * 2); // Update the hitbox based on the tower's position and range
+            }
+            else if (towerType == "Archer")
+            {
+                towerDamage = 5;
+                towerRange = 90;
+                towerHitbox = new Rectangle((int)towerPos.X - towerRange, (int)towerPos.Y - towerRange, (int)towerRange * 2, (int)towerRange * 2); // Update the hitbox based on the tower's position and range
             }
         }
 
         public void Update()
         {
+            DamageBasedOnLevel();
+            TowerType();
         }
 
         public void Draw(SpriteBatch spriteBatch)
