754369cb938a0e12263fb467ae7e8650c8cd5d4b
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-06-08 11:08:00 +0200

    Tower Constructor

--- Tower_Defence/Tower.cs
+++ Tower_Defence/Tower.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Spline;
 
 namespace Tower_Defence
 {
-    internal class Tower
+    public class Tower
     {
+        private Vector2 towerPos;
+        private Rectangle towerHitbox;
+        private Rectangle towerTexRec;
+        private Texture2D towerTexture;
+        private int towerDamage;
+        private int towerRange;
+        private int towerLevel;
+
+        public Tower(Texture2D tex, Vector2 pos, int damage, int range, Rectangle texRec) // Constructor for the Tower class
+        {
+            this.towerTexture = tex;
+            this.towerPos = pos;
+            this.towerDamage = damage;
+            this.towerRange = range;
+            this.towerTexRec = texRec;
+            this.towerLevel = 1;
+            towerHitbox = new Rectangle((int)range, (int)range, (int)range, (int)range); // Initialize the hitbox with the tower's range
+        }
     }
 }
